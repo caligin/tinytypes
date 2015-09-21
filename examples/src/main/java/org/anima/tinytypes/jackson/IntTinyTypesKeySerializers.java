188e2f647b0858b9b7b9cf9f4efed352308aea56
Tsukihara Caligin <caligin35@gmail.com>
2015-09-21 13:21:32 +0100

    ref: outer to inner, removed unused rawClass references in serializers

--- examples/src/main/java/org/anima/tinytypes/jackson/IntTinyTypesKeySerializers.java
+++ examples/src/main/java/org/anima/tinytypes/jackson/IntTinyTypesKeySerializers.java
@@ -1,10 +1,14 @@
 package org.anima.tinytypes.jackson;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.Serializers;
+import java.io.IOException;
 import org.anima.tinytypes.IntTinyType;
 
 public class IntTinyTypesKeySerializers extends Serializers.Base {
@@ -13,9 +17,18 @@ public class IntTinyTypesKeySerializers extends Serializers.Base {
     public JsonSerializer<?> findSerializer(SerializationConfig config, JavaType type, BeanDescription beanDesc) {
         Class<?> candidateTT = type.getRawClass();
         if (IntTinyType.class.equals(candidateTT.getSuperclass())) {
-            return new IntTinyTypeKeySerializer(candidateTT);
+            return new IntTinyTypeKeySerializer();
         }
         return super.findSerializer(config, type, beanDesc);
+    }
+
+    public static class IntTinyTypeKeySerializer<T extends IntTinyType> extends JsonSerializer<T> {
+
+        @Override
+        public void serialize(T value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
+            gen.writeFieldName(Integer.toString(value.value));
+        }
+
     }
 
 }
--- examples/src/main/java/org/anima/tinytypes/jackson/StringTinyTypesKeyDeserializers.java
+++ examples/src/main/java/org/anima/tinytypes/jackson/StringTinyTypesKeyDeserializers.java
@@ -1,11 +1,15 @@
 package org.anima.tinytypes.jackson;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.KeyDeserializer;
 import com.fasterxml.jackson.databind.deser.KeyDeserializers;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import org.anima.tinytypes.StringTinyType;
 
 public class StringTinyTypesKeyDeserializers implements KeyDeserializers {
@@ -17,6 +21,25 @@ public class StringTinyTypesKeyDeserializers implements KeyDeserializers {
             return new StringTinyTypeKeyDeserializer(candidateTT);
         }
         return null;
+    }
+
+    public static class StringTinyTypeKeyDeserializer extends KeyDeserializer {
+
+        private final Class<?> rawType;
+
+        public StringTinyTypeKeyDeserializer(Class<?> rawType) {
+            this.rawType = rawType;
+        }
+
+        @Override
+        public Object deserializeKey(String key, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            try {
+                return rawType.getConstructor(String.class).newInstance(key);
+            } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
     }
 
 }
