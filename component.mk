3f38ba2f23f82ff8ecfb51eb0269f918857a3b4a
Tsukihara Caligin <caligin35@gmail.com>
2015-10-29 23:58:47 +0000

    enh: also copying over and signing pom as releasable artifact, generating bundle for central upload

--- component.mk
+++ component.mk
@@ -2,11 +2,12 @@ version=$(shell mvn help:evaluate -Dexpression="project.version" | grep -v INFO)
 build_final_name=$(shell mvn help:evaluate -Dexpression="project.build.finalName" | grep -v INFO)
 sources=$(shell find src/main/ -name '*.java')
 tests=$(shell find src/test/ -name '*.java')
-jars=$(addprefix target/, $(addsuffix .jar, $(build_final_name) $(build_final_name)-sources $(build_final_name)-javadoc))
-signatures=$(addsuffix .asc, $(jars))
+pom=$(build_final_name).pom
+artifacts=$(addprefix target/, $(addsuffix .jar, $(build_final_name) $(build_final_name)-sources $(build_final_name)-javadoc) $(pom))
+signatures=$(addsuffix .asc, $(artifacts))
 
-.PHONY: clean install sign sources javadoc
-all: sign
+.PHONY: clean install sign sources javadoc pom bundle
+all: bundle
 
 target/:
 	mkdir -p target
@@ -27,11 +28,19 @@ javadoc: target/$(build_final_name)-javadoc.jar
 target/$(build_final_name)-javadoc.jar: pom.xml $(sources)
 	mvn javadoc:jar
 
+pom: target/$(pom.xml)
+target/$(pom): pom.xml
+	cp pom.xml $@
+
 %.asc: %
 	gpg -ab $<
 
 sign: $(signatures)
 $(signatures):
+
+bundle: target/bundle.jar
+target/bundle.jar: $(signatures) $(artifacts)
+	jar -cvf target/bundle.jar $(addprefix -C target/ , $(notdir $^))
 
 clean:
 	mvn clean
